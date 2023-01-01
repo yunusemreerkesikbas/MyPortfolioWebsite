844d296a090b9824f8b7b4eebd5d27f4004440d1
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2023-01-01 03:59:06 +0300

    delete project section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
-var swiper = new Swiper(".swiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  freeMode: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+
+$(function () {
+
+ 
+
+  /* Open Top Menu */
+  $('.page').on('click', '.menu-btn', function () {
+    if ($('.menu').hasClass('active')) {
+      $('.menu').removeClass('active');
+      $(this).removeClass('active');
+    } else {
+      $('.menu').addClass('active');
+      $(this).addClass('active');
+    }
+
+    return false;
+  });
+
+ 
+  var swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 });
